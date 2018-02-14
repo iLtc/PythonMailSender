174dc38a44aaac78272147e0cdd75a1eb8bf6742
iLtc <i@iltc.io>
2018-02-13 22:51:35 -0600

    Added Error Detection

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from sender import sender
 
 def main(event, context):
-    sender("i@iltc.io", "i@iltc.io", "Test Email", "This is our second email. We use it to test the function~")
+    status, msg = sender("i@iltc.io", "i@iltc.io", "Test Email", "This is our second email. We use it to test the function~")
 
-    response = {
-        "statusCode": 200,
-        "body": "OK"
-    };
-    return response
+    if status:
+        response = {
+            "statusCode": 200,
+            "body": "OK"
+        };
+        return response
+    else:
+        response = {
+            "statusCode": 500,
+            "body": msg
+        };
+        return response
