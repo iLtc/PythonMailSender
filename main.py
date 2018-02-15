ebff38fde36122d500be0076264c768f2c2b064e
iLtc <i@iltc.io>
2018-02-15 13:20:45 -0600

    Added Request

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from sender import sender
 
+
 def main(event, context):
+    request_strings = event['body'].split('&')
+    request = {}
+    for request_string in request_strings:
+        key, value = request_string.split('=')
+        request[key] = value
+
     status, msg = sender("i@iltc.io", "i@iltc.io", "Test Email", "This is our second email. We use it to test the function~")
 
     if status:
