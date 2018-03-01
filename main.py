0c855b76c20c8d8897dcd0570fd521215c8b517b
iLtc <i@iltc.io>
2018-02-28 22:56:08 -0600

    Finished Verifier

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from sender import sender
+from verifier import verifier
+import json
 
 
 def main(event, context):
@@ -7,6 +9,19 @@ def main(event, context):
     for request_string in request_strings:
         key, value = request_string.split('=')
         request[key] = value
+
+    if ('page' not in request) or (request['page'] not in ['HawkEvents']):
+        return response_error(400, "Unknown Page")
+
+    if 'g-recaptcha-response' not in request:
+        return response_error(400, "Google Recaptcha Response Not Found")
+
+    verify_result = verifier(request['page'], request['g-recaptcha-response'])
+
+    return {
+        "statusCode": 200,
+        "body": json.dumps(verify_result)
+    }
 
     status, msg = sender("i@iltc.io", "i@iltc.io", "Test Email", "This is our second email. We use it to test the function~")
 
@@ -22,3 +37,10 @@ def main(event, context):
             "body": msg
         }
         return response
+
+
+def response_error(status_code, body):
+    return {
+        "statusCode": status_code,
+        "body": body
+    }
--- verifier.py
+++ verifier.py
@@ -1,2 +1,10 @@
-def verifier():
-    pass
+import os
+import requests
+
+
+def verifier(page, response):
+    secret = os.environ.get(page + 'GOOGLE_RECAPTCHA_SECRET')
+    r = requests.post('https://www.google.com/recaptcha/api/siteverify', data={'secret': secret, 'response': response})
+    result = r.json()
+
+    return result
